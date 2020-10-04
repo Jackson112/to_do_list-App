03dc15f7db20b7ba3541a965a6919163a1c51c20
Jackson Lazaro <jacksonlayaroict@gmail.com>
2020-10-04 07:47:26 -0700

    Simple to do app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class  extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
